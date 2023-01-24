427e7b8680ad054856ee27a43b422b4d5f63c686
Luke Karrys <luke@lukekarrys.com>
2023-01-23 17:24:15 -0700

    fix: release integrtation

--- scripts/template-oss.js
+++ scripts/template-oss.js
@@ -8,14 +8,8 @@ module.exports = {
   rootRepo: {
     add: {
       '.github/workflows/post-pull-request.yml': './post-pull-request.yml',
-      '.github/workflows/release.yml': './release.yml',
       '.github/workflows/release-integration.yml': './release-integration.yml',
       '.github/workflows/node-integration.yml': './node-integration.yml',
-    },
-  },
-  workspaceRepo: {
-    add: {
-      '.github/workflows/release.yml': false,
     },
   },
 }
