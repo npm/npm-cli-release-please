7a0ca66fba87d2954b10ec6b90fb8d93b178e4de
Luke Karrys <luke@lukekarrys.com>
2023-01-26 14:42:51 -0700

    fix: new stuff

--- scripts/template-oss.js
+++ scripts/template-oss.js
@@ -6,12 +6,9 @@ module.exports = {
     '/AUTHORS',
   ],
   ciVersions: 'latest',
-  codeowner: '@lukekarrys',
   rootRepo: {
     add: {
       '.github/workflows/post-pull-request.yml': './post-pull-request.yml',
-      '.github/workflows/release-integration.yml': './release-integration.yml',
-      '.github/workflows/node-integration.yml': './node-integration.yml',
     },
   },
 }
