7ecf8a98ff97355b936d998ca285a35e472520b9
Luke Karrys <luke@lukekarrys.com>
2023-07-14 13:20:16 -0700

    chore: setup for mock testing

--- scripts/template-oss/index.js
+++ scripts/template-oss/index.js
@@ -3,6 +3,7 @@ module.exports = {
   windowsCI: false,
   macCI: false,
   ciVersions: 'latest',
+  branches: ['template-oss-mock-testing-branch-do-not-delete'],
   allowPaths: [
     '/AUTHORS',
     '/ALLOWED',
