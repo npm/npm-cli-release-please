092ac67f2c701fd4f9533886b353d80d638f3156
Luke Karrys <luke@lukekarrys.com>
2023-11-19 20:45:48 -0700

    chore: update template-oss

--- scripts/template-oss/index.js
+++ scripts/template-oss/index.js
@@ -12,12 +12,14 @@ module.exports = {
     add: {
       '.github/settings.yml': false,
       '.github/dependabot.yml': false,
+      '.github/workflows/release.yml': false,
     },
   },
   workspaceRepo: {
     add: {
       '.github/settings.yml': false,
       '.github/dependabot.yml': false,
+      '.github/workflows/release.yml': false,
     },
   },
 }
